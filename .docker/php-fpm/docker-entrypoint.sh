e1afaa3499e0872bb2bbc75b75e2cb675609687f
kacpergorec <kacpergorec@gmail.com>
2024-12-20 18:31:38 +0100

    init: initialize dockerized symfony skeleton app

---
+++ .docker/php-fpm/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Destination of env file inside container
+ENV_FILE="/var/www/.env"
+
+# Loop through XDEBUG, PHP_IDE_CONFIG and REMOTE_HOST variables and check if they are set.
+# If they are not set then check if we have values for them in the env file, if the env file exists. If we have values
+# in the env file then add exports for these in in the ~./bashrc file.
+for VAR in XDEBUG PHP_IDE_CONFIG REMOTE_HOST
+do
+  if [ -z "${!VAR}" ] && [ -f "${ENV_FILE}" ]; then
+    VALUE=$(grep $VAR $ENV_FILE | cut -d '=' -f 2-)
+    if [ ! -z "${VALUE}" ]; then
+      # Before adding the export we clear the value, if set, to prevent duplication.
+      sed -i "/$VAR/d"  ~/.bashrc
+      echo "export $VAR=$VALUE" >> ~/.bashrc;
+    fi
+  fi
+done
+
+# If there is still no value for the REMOTE_HOST variable then we set it to the default of host.docker.internal. This
+# value will be sufficient for windows and mac environments.
+if [ -z "${REMOTE_HOST}" ]; then
+  REMOTE_HOST="host.docker.internal"
+  sed -i "/REMOTE_HOST/d"  ~/.bashrc
+  echo "export REMOTE_HOST=\"$REMOTE_HOST\"" >> ~/.bashrc;
+fi
+
+# Source the .bashrc file so that the exported variables are available.
+. ~/.bashrc
+
+exec "$@"
