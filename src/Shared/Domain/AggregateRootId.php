69f3a4ebf7b4ecb869c8d744df6eface9662044d
kacpergorec <kacpergorec@gmail.com>
2024-12-20 19:54:21 +0100

    feat: add domain abstraction

---
+++ src/Shared/Domain/AggregateRootId.php
@@ -0,0 +1,41 @@
+<?php
+declare (strict_types=1);
+
+namespace App\Shared\Domain;
+
+use Symfony\Component\Uid\Uuid;
+
+abstract class AggregateRootId implements \JsonSerializable
+{
+    protected Uuid $uuid;
+
+    public function __construct(Uuid $uuid)
+    {
+        $this->uuid = $uuid;
+    }
+
+    public function toUuid(): Uuid
+    {
+        return $this->uuid;
+    }
+
+    public static function fromString(string $uuid): static
+    {
+        return new static(Uuid::fromString($uuid));
+    }
+
+    public function jsonSerialize(): string
+    {
+        return static::toUuid()->toString();
+    }
+
+    public function __toString(): string
+    {
+        return $this->uuid->toString();
+    }
+
+    public static function new(): static
+    {
+        return new static(Uuid::v7());
+    }
+}
